79e599e4b1168380681435ecdd5615d5823170c7
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2024-12-20 22:08:53 -0500

    Play music in the background.

--- main.ts
+++ main.ts
@@ -1,159 +1,12 @@
 /**
  * https://musescore.com/user/35461455/scores/10951453
+ * 
+ * Main theme = measures 1 through 16.
+ * Timer = measures 17 through 32.
  */
-function playMainTheme() {
-    let measureCount: number = 1
-    for (let measure of mainThemeMeasures) {
-        info.setScore(measureCount)
-        if (measure.length == 8) {
-            music.play(mml.playable(
-                mml.track(mml.Dog, measure[0]),
-                mml.track(mml.Dog, measure[1]),
-                mml.track(mml.Dog, measure[2]),
-                mml.track(mml.Dog, measure[3]),
-                mml.track(mml.Dog, measure[4]),
-                mml.track(mml.Dog, measure[5]),
-                mml.track(mml.Dog, measure[6]),
-                mml.track(mml.Dog, measure[7]),
-            ), music.PlaybackMode.UntilDone)
-        } else if (measure.length == 6) {
-            music.play(mml.playable(
-                mml.track(mml.Dog, measure[0]),
-                mml.track(mml.Dog, measure[1]),
-                mml.track(mml.Dog, measure[2]),
-                mml.track(mml.Dog, measure[3]),
-                mml.track(mml.Dog, measure[4]),
-                mml.track(mml.Dog, measure[5]),
-            ), music.PlaybackMode.UntilDone)
-        } else if (measure.length == 4) {
-            music.play(mml.playable(
-                mml.track(mml.Dog, measure[0]),
-                mml.track(mml.Dog, measure[1]),
-                mml.track(mml.Dog, measure[2]),
-                mml.track(mml.Dog, measure[3]),
-            ), music.PlaybackMode.UntilDone)
-        } else if (measure.length == 3) {
-            music.play(mml.playable(
-                mml.track(mml.Dog, measure[0]),
-                mml.track(mml.Dog, measure[1]),
-                mml.track(mml.Dog, measure[2])
-            ), music.PlaybackMode.UntilDone)
-        }
-        measureCount++
-    }
-}
-function playTimer() {
-    info.startCountdown(30)
-    let measureCount: number = 17
-    for (let measure of timerMeasures) {
-        info.setScore(measureCount)
-        if (measure.length == 2) {
-            music.play(mml.playable(
-                mml.track(mml.Dog, measure[0]),
-                mml.track(mml.Dog, measure[1]),
-            ), music.PlaybackMode.UntilDone)
-        } else {
-            music.play(mml.playable(
-                mml.track(mml.Dog, measure[0]),
-                mml.track(mml.Dog, measure[1]),
-                mml.track(mml.Dog, measure[2]),
-                mml.track(mml.Dog, measure[3]),
-            ), music.PlaybackMode.UntilDone)
-        }
-        measureCount++
-    }
-}
-info.onCountdownEnd(function () {
-
-})
-let timerMeasures: string[][] = []
-timerMeasures = [
-    // Measure 17
-    [
-        "L16 O4 C8 C > C R < C > C8 < C8 C > C P < C > C8",
-        "L1 O3  C",
-    ],
-    // Measure 18
-    [
-        "L16 O4 C8 C > C R < C > C8 < C8 C > C P < C > C C",
-        "L1 O3  C",
-    ],
-    // Measure 19
-    [
-        "L16 O4 C8 C > C R < C > C8 < C8 C > C P < C > C8",
-        "L1 O2  C",
-    ],
-    // Measure 20
-    [
-        "L16 O4 C8 C > C R < C > C8 < C8 C > C P < C > C C",
-        "L1 O2  C",
-    ],
-    // Measure 21
-    [
-        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D8",
-        "L1 O2  C",
-    ],
-    // Measure 22
-    [
-        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D D",
-        "L1 O2  C",
-    ],
-    // Measure 23
-    [
-        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D8",
-        "L1 O2  C",
-    ],
-    // Measure 24
-    [
-        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D D",
-        "L1 O2  C",
-    ],
-    // Measure 25
-    [
-        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D8",
-        "L1 O2  C",
-    ],
-    // Measure 26
-    [
-        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D D",
-        "L1 O2  C",
-    ],
-    // Measure 27
-    [
-        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D8",
-        "L1 O2  C",
-    ],
-    // Measure 28
-    [
-        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D D",
-        "L1 O2  C",
-    ],
-    // Measure 29
-    [
-        "L16 O4 D8 D8 < G > D8 P D8 D8 < G > D8 P",
-        "L4 O2  C       C        C       C",
-    ],
-    // Measure 30
-    [
-        "L4 O4  D",
-        "L16 O2 C4 C4 A A P A B- B- < A",
-    ],
-    // Measure 31
-    [
-        "L8 O3 D. F. F# G. A-. A",
-        "L8 O2 D. F. F# G. A-. A",
-    ],
-    // Measure 32
-    [
-        "L16 O5 D C P8 < G A P8 > G F D F P8 F",
-        "L16 O4 D C P8 < G A P8 > G F D F P8 D",
-        "L16 O3 D C P8 < G A P8 > G F D F P8 D",
-        "L16 O2 D C P8 < G A P8 > G F D F P8 D",
-    ],
-]
-
-let mainThemeMeasures: string[][] = []
-mainThemeMeasures = [
+const COUNTDOWN_MML: string[][] = [
+    // Measure 0
+    [],
     // Measure 1
     [
         "L1 O4 C",
@@ -294,8 +147,153 @@ mainThemeMeasures = [
         "L16 O4 C<B- P8 F G P8>F E- C E- P8 C8",
         "L16 O3 C<B- P8 F G P8>F E- C E- P8 C8",
         "L16 O2 C<B- P8 F G P8>F E- C E- P8 C8",
-    ]
+    ],
+    // Measure 17
+    [
+        "L16 O4 C8 C > C R < C > C8 < C8 C > C P < C > C8",
+        "L1 O3  C",
+    ],
+    // Measure 18
+    [
+        "L16 O4 C8 C > C R < C > C8 < C8 C > C P < C > C C",
+        "L1 O3  C",
+    ],
+    // Measure 19
+    [
+        "L16 O4 C8 C > C R < C > C8 < C8 C > C P < C > C8",
+        "L1 O2  C",
+    ],
+    // Measure 20
+    [
+        "L16 O4 C8 C > C R < C > C8 < C8 C > C P < C > C C",
+        "L1 O2  C",
+    ],
+    // Measure 21
+    [
+        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D8",
+        "L1 O2  C",
+    ],
+    // Measure 22
+    [
+        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D D",
+        "L1 O2  C",
+    ],
+    // Measure 23
+    [
+        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D8",
+        "L1 O2  C",
+    ],
+    // Measure 24
+    [
+        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D D",
+        "L1 O2  C",
+    ],
+    // Measure 25
+    [
+        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D8",
+        "L1 O2  C",
+    ],
+    // Measure 26
+    [
+        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D D",
+        "L1 O2  C",
+    ],
+    // Measure 27
+    [
+        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D8",
+        "L1 O2  C",
+    ],
+    // Measure 28
+    [
+        "L16 O4 D8 D > D R < D > D8 < D8 D > D P < D > D D",
+        "L1 O2  C",
+    ],
+    // Measure 29
+    [
+        "L16 O4 D8 D8 < G > D8 P D8 D8 < G > D8 P",
+        "L4 O2  C       C        C       C",
+    ],
+    // Measure 30
+    [
+        "L4 O4  D",
+        "L16 O2 C4 C4 A A P A B- B- < A",
+    ],
+    // Measure 31
+    [
+        "L8 O3 D. F. F# G. A-. A",
+        "L8 O2 D. F. F# G. A-. A",
+    ],
+    // Measure 32
+    [
+        "L16 O5 D C P8 < G A P8 > G F D F P8 F",
+        "L16 O4 D C P8 < G A P8 > G F D F P8 D",
+        "L16 O3 D C P8 < G A P8 > G F D F P8 D",
+        "L16 O2 D C P8 < G A P8 > G F D F P8 D",
+    ],
 ]
+
+let isPlaying: boolean = false
+let nextMeasureTime: number = 0
+let currentMeasure: number = 0
+let currentTempo: number = 0
+let beatsPerMeasure: number = 0
+let measureMs: number = 0
+let firstMeasure: number = 0
+let lastMeasure: number = 0
+let instrument: mml.MMLInstrument = mml.Dog
+
+game.onUpdate(() => {
+    if (isPlaying && game.runtime() >= nextMeasureTime) {
+        // Time to play the next measure.
+        if (currentMeasure == -1) {
+            currentMeasure = firstMeasure
+        }
+        if (currentMeasure > lastMeasure || currentMeasure >= COUNTDOWN_MML.length) {
+            isPlaying = false
+            info.setScore(0)
+            return
+        }
+        info.setScore(currentMeasure)
+        let tracks: mml.MMLTrack[] = []
+        for (let t of COUNTDOWN_MML[currentMeasure]) {
+            let track: mml.MMLTrack = new mml.MMLTrack(t, instrument)
+            tracks.push(track)
+        }
+        // Allow for an empty measure.
+        if (tracks.length > 0) {
+            music.play(new mml.MMLPlayable(tracks), music.PlaybackMode.InBackground)
+        }
+        currentMeasure++
+        nextMeasureTime = game.runtime() + measureMs
+    }
+})
+
+function playMainTheme() {
+    nextMeasureTime = 0
+    currentMeasure = -1
+    currentTempo = 120
+    beatsPerMeasure = 4
+    measureMs = 60000 * beatsPerMeasure / currentTempo
+    firstMeasure = 1
+    lastMeasure = 16
+    isPlaying = true
+}
+
+function playTimer() {
+    nextMeasureTime = 0
+    currentMeasure = -1
+    currentTempo = 120
+    beatsPerMeasure = 4
+    measureMs = 60000 * beatsPerMeasure / currentTempo
+    firstMeasure = 17
+    lastMeasure = 32
+    isPlaying = true
+    info.startCountdown(30)
+}
+
+info.onCountdownEnd(function () {
+
+})
 
 interface Point {
     x: number
@@ -343,15 +341,6 @@ function floodScanline(img: Image, x: number, y: number, c: number) {
         // pause(50)
     }   // while (stack)
 }   // floodScanline()
-
-function runOpening(): void {
-    for (let i: number = 0; i < 2; i++) {
-        playMainTheme()
-        music.play(mml.playable(
-            mml.track(mml.Dog, "P1")
-        ), music.PlaybackMode.UntilDone)
-    }
-}
 
 function drawClock(i: Image, percent: number) {
     let c: number = i.width / 2
@@ -404,13 +393,13 @@ game.onUpdate(() => {
             info.setScore(Math.floor(elapsed))
             drawClock(clockImage, p)
         } else {
-            music.play(music.melodyPlayable(music.buzzer), music.PlaybackMode.UntilDone)
+            // music.play(music.melodyPlayable(music.buzzer), music.PlaybackMode.UntilDone)
             clockRunning = false
         }
     }
 })
 
 scene.setBackgroundColor(9)
-runOpening()
-// playTimer()
-// startClock()
+// playMainTheme()
+playTimer()
+startClock()
